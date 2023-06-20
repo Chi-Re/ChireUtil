e7e0d85ec528ee7d87bc4204c5cc56225e42c130
Administrator <3531962676@qq.com>
2023-06-21 02:07:09 +0800

    chire

--- python/src/chireutil/python/Python.java
+++ python/src/chireutil/python/Python.java
@@ -7,6 +7,9 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
+/**@author 炽热S
+ * 可以实现python的使用
+ */
 public class Python {
     public PythonInterpreter interpreter;
     public Python(PythonInterpreter interpreter){
@@ -15,17 +18,21 @@ public class Python {
     public Python(){
         this.interpreter = init();
     }
+    /**初始化，为了方便*/
     public static PythonInterpreter init() {
         try (PythonInterpreter inter = new PythonInterpreter()){
             return inter;
         }
     }
+    /**打印变量*/
     public void printIn(String arg) {
         interpreter.exec("print(" + arg + ");");
     }
+    /**打印字*/
     public void print(String arg){
         printIn("'" + arg + "'");
     }
+    /**调用项目内的py文件(所有都可以)，返回文件内容*/
     public String putFile(String file, Class<?> c){
         try {
             ClassLoader classLoader = c.getClassLoader();
